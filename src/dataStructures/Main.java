cae80cf19fa3f890cbf8e50794293a4ef4a216ab
Elijah Munroe <elimunroe15@hotmail.com>
2021-02-03 02:15:42 -0500

    Produce sum of values in array

--- src/dataStructures/Main.java
+++ src/dataStructures/Main.java
@@ -3,6 +3,16 @@ package dataStructures;
 public class Main {
 
     public static void main(String[] args) {
+        // Part 1
+        int[] randomArray = {1, 2, 3, 4, 5};
+        System.out.printf("Sum of array: %d", sumArray(randomArray));
+    }
 
+    public static int sumArray (int[] array){
+        int sum = 0;
+        for (int i : array) {
+            sum += i;
+        }
+        return sum;
     }
 }
