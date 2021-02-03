0a49c4d386544d9f70aa33b636c0d286d281e1b9
Elijah Munroe <elimunroe15@hotmail.com>
2021-02-03 02:18:30 -0500

    Run output of given program

--- src/dataStructures/Main.java
+++ src/dataStructures/Main.java
@@ -5,7 +5,19 @@ public class Main {
     public static void main(String[] args) {
         // Part 1
         int[] randomArray = {1, 2, 3, 4, 5};
-        System.out.printf("Sum of array: %d", sumArray(randomArray));
+        System.out.printf("Sum of array: %d\n", sumArray(randomArray));
+
+        // Part 2
+        double[] exampleArray = {1,5,6,5,4,1};
+        double maximum = exampleArray[0];
+        int index = 0;
+        for (int i = 1; i< exampleArray.length; i++){
+            if (exampleArray[ i ] > maximum) {
+                maximum = exampleArray[ i ];
+                index = i;
+            }
+        }
+        System.out.println("Index: " + index);
     }
 
     public static int sumArray (int[] array){
